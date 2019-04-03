b94981b75425755fd738b818c1e2111517fe01b4
Paul Williamsen <pwilliamsen@me.com>
2019-04-03 15:34:21 -0700

    radix work

---
+++ code-challanges/radix/radix.py
@@ -0,0 +1,26 @@
+"""
+radix sort
+"""
+
+def radix_sort(list):
+    queues = [Queue() for i in range(0,10)]
+    max = max(list)
+    iter = len(str(max))
+    _pass = 1
+
+    def _radix_sort(list):
+        while _pass < iter + 1:
+            output = []
+
+            for j in range(len(list)):
+                if list[j] % 10^_pass == j:
+                    queues[j].enqueue(list[j])
+            for q in queues:
+                while q:
+                    output.append(q.dequeue)
+            _pass += 1
+
+            return _radix_sort(output)
+        return list
+
+
