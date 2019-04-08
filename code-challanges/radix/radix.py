0160f82d7c4dbff5f2ef21e1be8305a9ddd0174d
Paul Williamsen <pwilliamsen@me.com>
2019-04-08 14:25:29 -0700

    radix work

--- code-challanges/radix/radix.py
+++ code-challanges/radix/radix.py
@@ -1,26 +1,38 @@
 """
 radix sort
 """
+from import Queue, Node
 
 def radix_sort(list):
     queues = [Queue() for i in range(0,10)]
-    max = max(list)
+    max = max(list) if len(list) > 0 else 0
     iter = len(str(max))
-    _pass = 1
+    _pass = 0
 
     def _radix_sort(list):
-        while _pass < iter + 1:
+        """
+        
+        """
+        nonlocal iter
+        nonlocal _pass
+
+        while _pass < iter:
             output = []
 
-            for j in range(len(list)):
-                if list[j] % 10^_pass == j:
-                    queues[j].enqueue(list[j])
+            for j in list:
+                q = j // (10 ** _pass) % 10
+                queues[q].enqueue(j)
+
             for q in queues:
-                while q:
-                    output.append(q.dequeue)
+                while q.front:
+                    output.append(q.dequeue())
+
             _pass += 1
 
             return _radix_sort(output)
+
         return list
+
+    return _radix_sort(list)
 
 
